cc4c9ec0867f5f3cc61ca6ff7553e1b91504e23f
Anton Laptop <magicdude4@gmail.com>
2023-09-02 16:15:50 +0300

    finished calc.py

--- calc.py
+++ calc.py
@@ -1,8 +1,7 @@
 from enum import Enum
 import logging
 
-logging.basicConfig(level=logging.ERROR,filename='calc_error.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-logging.basicConfig(level=logging.INFO,filename='calc_info.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, filename='calc_info.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 class Actions(Enum):
     MULTIPLICATION = 1
@@ -11,42 +10,56 @@ class Actions(Enum):
     EXIT = 4
 
 def menu():
-    global products,cart
-    while(True):
+    while True:
         for action in Actions:
             print(f"{action.value} - {action.name}")
-        user_selection=Actions(int(input("What would you like to do? ")))
+        user_selection = Actions(int(input("What would you like to do? ")))
+        
         if user_selection == Actions.MULTIPLICATION:
-            mult()  
-        if user_selection == Actions.DIVISION: 
+            mult()
+        elif user_selection == Actions.DIVISION:
             div()
-        if user_selection == Actions.ADDITION: 
+        elif user_selection == Actions.ADDITION:
             add()
-        if user_selection == Actions.EXIT: return
+        elif user_selection == Actions.EXIT:
+            break
 
 def mult():
     print("Choose two numbers: ")
-    n1=input("1st number: ")
-    n2=input("2nd number: ")
+    n1 = float(input("1st number: "))
+    n2 = float(input("2nd number: "))
     try:
-        sum = n1*n2
+        result = n1 * n2
+        logging.info("Multiplication result: %f", result)
+        print(f"{n1} * {n2} = {result}")
     except Exception as e:
-        logging.INFO(exc_info=True)
-    return sum
+        logging.error("Error during multiplication: %s", str(e))
 
-def div(n1,n2):
+def div():
     print("Choose two numbers: ")
-    n1=input("1st number: ")
-    n2=input("2nd number: ")
+    n1 = float(input("1st number: "))
+    n2 = float(input("2nd number: "))
     try:
-        sum = n1/n2
+        if n2 != 0:
+            result = n1 / n2
+            logging.info("Division result: %f", result)
+            print(f"{n1} / {n2} = {result}")
+        else:
+            print("Cannot divide by zero.")
+            logging.error("Cannot divide by zero.")
     except Exception as e:
-        logging.error("Exception occurred", exc_info=True)
-    return sum
+        logging.error("Error during division: %s", str(e))
 
-def add(n1,n2):
+def add():
     print("Choose two numbers: ")
-    n1=input("1st number: ")
-    n2=input("2nd number: ")
-    sum = n1+n2
-    return sum
+    n1 = float(input("1st number: "))
+    n2 = float(input("2nd number: "))
+    try:
+        result = n1 + n2
+        logging.info("Addition result: %f", result)
+        print(f"{n1} + {n2} = {result}")
+    except Exception as e:
+        logging.error("Error during addition: %s", str(e))
+
+if __name__ == "__main__":
+    menu()
